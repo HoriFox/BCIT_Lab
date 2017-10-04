e8f2cd63d4d746116df0af53ae455fd447c41bf5
HeliSRaiN <lhelisrainl@gmail.com>
2017-10-04 17:07:44 +0300

    Набросок Lab_2

--- Lab_1/ProjectLaba/Program.cs
+++ Lab_1/ProjectLaba/Program.cs
@@ -1,61 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System; // О - Оптимизация :)
 
 namespace ProjectLaba
 {
     class Program
     {
+        static double inputVal(string prompt) // Или как вариант - передавать по ref
+        {
+            double a = 0;
+            do
+                Console.Write(prompt);
+            while (!double.TryParse(Console.ReadLine(), out a));
+            return a;
+        }
+
         static void Main(string[] args)
         {
             double a, b, c;
-            bool work = true;
-            while (work)
+            bool work;
+            do
             {
                 Console.Clear();
 
-                do
-                    Console.Write("Enter A: ");
-                while (!double.TryParse(Console.ReadLine(), out a));
-                a = Convert.ToInt32(a);
-
-                do
-                    Console.Write("Enter B: ");
-                while (!double.TryParse(Console.ReadLine(), out b));
-                a = Convert.ToInt32(b);
-
-                do
-                Console.Write("Enter C: ");
-                while (!double.TryParse(Console.ReadLine(), out c)) ;
-                a = Convert.ToInt32(c);
+                a = inputVal("Enter A: ");
+                b = inputVal("Enter B: "); // Домашнее задание: внести a, b, c - в массив и ввод в цикл
+                c = inputVal("Enter C: ");
 
                 double discriminant = (b * b) - 4 * a * c;
 
                 if (discriminant < 0)
                 {
-                    Console.Write("This equation has no solution");
+                    Console.Write("This equation has no solution"); // Точнее - irrational solution
                 }
                 else if (discriminant == 0)
                 {
-                    double x = (- b + Math.Round(Math.Sqrt(discriminant), 2)) / 2 * a;
+                    double x = (-b + Math.Round(Math.Sqrt(discriminant), 2)) / (2 * a);
                     Console.Write("X = " + x);
                 }
                 else if (discriminant > 0)
                 {
-                    double x1 = (-b + Math.Round(Math.Sqrt(discriminant), 2)) / 2 * a;
-                    double x2 = (-b - Math.Round(Math.Sqrt(discriminant), 2)) / 2 * a;
+                    double x1 = (-b + Math.Round(Math.Sqrt(discriminant), 2)) / (2 * a);
+                    double x2 = (-b - Math.Round(Math.Sqrt(discriminant), 2)) / (2 * a);
                     Console.Write("X1 = " + x1 + " X2 = " + x2);
                 }
 
-                Console.Write("\nExit? (1/0) \n");
-                a = Convert.ToInt32(Console.ReadLine());
-                if (a == 1)
-                {
-                    work = false;
-                }
-            }
+                Console.Write("\nExit? (1/0) \n"); // Можно заменить на человеческое [y/n]
+                string s = Console.ReadLine();     // Просто потому что это же C#
+                work = (s == "0" || s == "");      // Тогда: work = (s == "n" || s == "");
+            } while (work);
         }
     }
 }
--- Lab_2/ProjectLaba/ProjectLaba/Program.cs
+++ Lab_2/ProjectLaba/ProjectLaba/Program.cs
@@ -2,24 +2,56 @@
 
 namespace ProjectLaba
 {
-    class ГеометрическаяФигура
+    interface IPrint
     {
-        public virtual void getS()
+        void Print();
+    }
+    public abstract class GeometricFigure
+    {
+        public virtual void getS() // Получить площадь данной фигуры
+        {
+            Console.Write("This is area");
+        }
+    }
+    public class Rectangle : GeometricFigure, IPrint // Прямоугольник
+    {
+        private double width;
+        private double height;
+
+        public Rectangle(double width, double height) { }
+        public override void getS() { }
+        public virtual void Print()
+        {
+            Console.Write("Something print Recrangle");
+        }
+    }
+    public class Square : Rectangle // Квадрат
+    {
+        private double side;
+
+        public Square(double side) : base(side, side) { }
+        public override void getS() { }
+        public override void Print()
+        {
+            Console.Write("Something print Squere");
+        }
+    }
+    public class Circle : GeometricFigure // Круг
+    {
+        private double radius;
+
+        public Circle(double radius) { }
+        public override void getS() { }
+        public virtual void Print()
+        {
+            Console.Write("Something print Circle");
+        }
+    }
+    public class Program
+    {
+        static void Main()
         {
 
         }
-    }
-    class Прямоугольник : ГеометрическаяФигура
-    {
-        private double ширина;
-        private double высота;
-
-        public Прямоугольник(double ширина, double высота) { }
-    }
-    class Квадрат : Прямоугольник
-    {
-        private double сторона;
-
-        public Квадрат(double строна, double сторона) { }
     }
 }
