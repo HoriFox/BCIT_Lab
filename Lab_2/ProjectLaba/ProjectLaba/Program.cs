c9a29421ae648f063f47023294601c3eea974083
HeliSRaiN <lhelisrainl@gmail.com>
2017-10-04 15:59:02 +0300

    Мелкие фиксы

---
+++ Lab_2/ProjectLaba/ProjectLaba/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ProjectLaba
+{
+    class ГеометрическаяФигура
+    {
+        public virtual void getS()
+        {
+
+        }
+    }
+    class Прямоугольник : ГеометрическаяФигура
+    {
+        private double ширина;
+        private double высота;
+
+        public Прямоугольник(double ширина, double высота) { }
+    }
+    class Квадрат : Прямоугольник
+    {
+        private double сторона;
+
+        public Квадрат(double строна, double сторона) { }
+    }
+}
