9c6fc57a45a6d462250ea1eaf194168fdef88e9a
HeliSRaiN <lhelisrainl@gmail.com>
2017-12-15 02:55:03 +0300

    Добавлена 3, 4 и 5 лаба

---
+++ Lab_4/ProjectLaba/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Lab_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadb = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.findb = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Кнопка загрузки новых данных
+            // 
+            this.loadb.Location = new System.Drawing.Point(28, 12);
+            this.loadb.Name = "loadb";
+            this.loadb.Size = new System.Drawing.Size(120, 23);
+            this.loadb.TabIndex = 0;
+            this.loadb.Text = "Загрузить данные";
+            this.loadb.UseVisualStyleBackColor = true;
+            this.loadb.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Окно разобранных слов
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(28, 48);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(200, 300);
+            this.listBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(255, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(255, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 3;
+            // 
+            // Кнопка поиска текста
+            // 
+            this.findb.Location = new System.Drawing.Point(411, 12);
+            this.findb.Name = "findb";
+            this.findb.Size = new System.Drawing.Size(120, 23);
+            this.findb.TabIndex = 5;
+            this.findb.Text = "Найти слово";
+            this.findb.UseVisualStyleBackColor = true;
+            this.findb.Click += new System.EventHandler(this.findb_Click);
+            // 
+            // Строка поиска слова
+            // 
+            this.textBox1.Location = new System.Drawing.Point(256, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // Главное окно
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 350);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.findb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.loadb);
+            this.Name = "Form1";
+            this.Text = "Lab_4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadb;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button findb;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Lab_5/ProjectLaba/ProjectLaba/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ProjectLaba
+{
+    public abstract class GeometricFigure : IComparable
+    {
+        public abstract double area();         // Площадь фигуры
+        public int CompareTo(object figure)
+        {
+            GeometricFigure figure2 = (GeometricFigure)figure;
+            if (this.area() < figure2.area())
+                return -1;
+            else if (this.area() > figure2.area())
+                return 1;
+            else return 0;
+        }
+    }
+
+    public class Rectangle : GeometricFigure // Прямоугольник
+    {
+        double width;
+        double height;
+        public override double area() { return width * height; }
+        public Rectangle (double _width, double _height)
+        {
+            width = _width;
+            height = _height;
+        }
+    }
+    public class Square : Rectangle         // Квадрат
+    {
+        public Square(double _side) : base(_side, _side) { }
+    }
+    public class Circle : GeometricFigure   // Круг
+    {
+        double radius;
+        public override double area() { return Math.PI * radius * radius; }
+        public Circle(double _radius)
+        {
+            radius = _radius;
+        }
+    }
+
+    public class Program
+    {
+        static double inputDouble() // Проверка на введённый Double
+        {
+            double a = 0;
+            double.TryParse(Console.ReadLine(), out a);
+            return a;
+        }
+        static int inputInt()       // Проверка на введённый Int
+        {
+            int a = 0;
+            int.TryParse(Console.ReadLine(), out a);
+            return a;
+        }
+        static void Main()
+        {
+            bool work;
+            do
+            {
+                // Меню
+
+                Console.Write("ArrayList: \n");
+                ArrayList collection = new ArrayList();
+                collection.Add(new Rectangle(20, 40));
+                collection.Add(new Rectangle(30, 40));
+                collection.Add(new Rectangle(80, 40));
+                collection.Sort();
+                foreach (GeometricFigure p in collection)
+                {
+                    Console.Write(p.area()+"\n");
+                }
+
+                Console.Write("\nList<GeometricFigure>: \n");
+                List<GeometricFigure> collection2 = new List<GeometricFigure>();
+                collection2.Add(new Rectangle(40, 40));
+                collection2.Add(new Rectangle(80, 40));
+                collection2.Add(new Rectangle(40, 45));
+                collection2.Sort();
+                foreach (GeometricFigure p in collection2)
+                {
+                    Console.Write(p.area() + "\n");
+                }
+
+
+                Console.Write("\nContinue? (y/n) \n");
+                string s = Console.ReadLine();     
+                work = (s == "y" || s == "");
+            } while (work);
+        }
+    }
+}
